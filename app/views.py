12135f16ddf51cf6f010c99cb466caf52a01c265
pavan10 <pavankumarreddy646@gmail.com>
2023-11-10 19:01:39 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+def first(request):
+    return render(request,'first.html')
+
+def second(request):
+    return render(request,'second.html')
+
+def third(request):
+    return render(request,'third.html')
+
+def four(request):
+   return render(request,'four.html')
